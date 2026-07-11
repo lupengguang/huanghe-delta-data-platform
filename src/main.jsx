8687938bbe228406b0d11f91f60e0c06f906fff7
lupengguang <2757016293@qq.com>
2026-07-11 23:55:47 +0800

    fix: 移除main.jsx中的重定向逻辑，改用404.html直接加载index.html

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const urlParams = new URLSearchParams(window.location.search);
-const redirectPath = urlParams.get('p');
-if (redirectPath && !window.location.hash) {
-  window.location.replace(redirectPath + window.location.hash);
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
